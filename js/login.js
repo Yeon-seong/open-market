e0246b39ceea819117d2a29cbd1f582afed399a5
yeon-seong <dd300@naver.com>
2024-08-08 14:20:55 +0900

    로그인 페이지 : 로그인 버튼 클릭 시 에러메시지를 폼 내에 표시

--- js/login.js
+++ js/login.js
@@ -4,3 +4,14 @@ document.querySelectorAll('.login_tab_button').forEach(button => {
     button.classList.add('active');
   });
 });
+
+document.querySelector('.login_button').addEventListener('click', function() {
+  let id = document.querySelector('.login_input[type="text"]').value;
+  let password = document.querySelector('.login_input[type="password"]').value;
+  
+  if (id === '' || password === '') {
+    document.querySelector('.same_error_message').style.display = 'block';
+  } else {
+    document.querySelector('.same_error_message').style.display = 'none';
+  }
+});
