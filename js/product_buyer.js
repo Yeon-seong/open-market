f29e88b8cc99c3548d40b820d8efa21406264303
yeon-seong <dd300@naver.com>
2024-08-09 13:19:55 +0900

    상품 목록 페이지 : 상품 전체 불러오기

---
+++ js/product_buyer.js
@@ -0,0 +1,38 @@
+const $productContainer = document.querySelector('.product_container');
+async function productPrint() {
+  try {
+    const response = await fetch('https://openmarket.weniv.co.kr/products/');
+    const data = await response.json();
+
+    data.results.forEach(function(goods) {
+      const productItem = createProductItem(goods);
+      $productContainer.appendChild(productItem);
+    });
+
+  } catch (error) {
+    alert("상품을 불러오는 중 오류가 발생했습니다.");
+    console.error('상품을 불러오는 중 오류가 발생했습니다', error);
+  }
+}
+
+
+
+function createProductItem(goods) {
+  const productItem = document.createElement('div');
+  productItem.className = 'product_list_center';
+  productItem.innerHTML = 
+  `
+    <figure class="product_figure">
+      <img src="${goods.image}" class="product_image" alt="${goods.product_name}" />
+      <figcaption class="product_store_name">${goods.store_name}</figcaption>
+    </figure>
+
+    <h2 class="product_title_name">${goods.product_name}</h2>
+    <p class="product_price">
+      ${goods.price.toLocaleString()}
+      <span class="product_price_unit">원</span>
+    </p>
+  `;
+  return productItem;
+}
+document.addEventListener('DOMContentLoaded', productPrint);
